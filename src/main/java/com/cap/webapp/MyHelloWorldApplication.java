94f2281e323acba04c206119f0e91e8f072a5672
sujodey <60807041+sujodey@users.noreply.github.com>
2020-07-11 21:45:16 +0530

    Add files via upload

---
+++ src/main/java/com/cap/webapp/MyHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.cap.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cap/webapp/MyHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cap.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
